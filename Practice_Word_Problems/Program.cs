b7e5920e59d74d2686a7fe54a1f7d1776872e0c7
DmitriC123 <cohan.dima@gmail.com>
2017-09-27 11:19:56 -0400

    initial commit.

---
+++ Practice_Word_Problems/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Word_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MikePay = 13;
+            Console.WriteLine(MikePay * 4);
+
+            int Sugar = 7;
+            Console.WriteLine(Sugar - 2);
+
+            int Diapers = 40;
+            Console.WriteLine(Diapers / 8);
+
+            int TrevonCash = 41;
+            Console.WriteLine(TrevonCash - 29);
+
+            int Julia = 47;
+            Console.WriteLine(Julia + 30);
+
+            int Envelopes = 12;
+            Console.WriteLine(Envelopes / 3);
+
+            float Cash = 27.10f;
+            float Bill = 5.12f;
+            Console.WriteLine(Cash + Bill);
+
+            int Students = 324;
+            Console.WriteLine(Students / 6);
+
+
+
+
+        }
+    }
+}
